e57abb8b4b5bf16de6d51a08e7af54ff5064605e
Ale Sánchez <alesanchezmedina@gmail.com>
2016-02-06 11:02:58 +0100

    Finalizado el sistema de escucha

--- src/main/scala/TwitterScrapper.scala
+++ src/main/scala/TwitterScrapper.scala
@@ -45,16 +45,7 @@ object TwitterScrapper {
 
     // Your code goes here
     val conf = new SparkConf().setAppName("TwitterScrapper")
-    val ssc = new StreamingContext(conf, Seconds(5))    
-      
-    sys.addShutdownHook({
-      println("ShutdownHook called")
-      ssc.stop(true, true)
-
-      while (!ssc.getState().equals(StreamingContextState.STOPPED)) {
-        println("Stopping... - " +  ssc.getState().toString())
-      }
-    })
+    val ssc = new StreamingContext(conf, Seconds(5))
     
     var tweets = TwitterUtils.createStream(ssc, None)
     var statuses = tweets.map { status => (status.getText, status.getCreatedAt, status.getLang) }
@@ -72,7 +63,7 @@ object TwitterScrapper {
       
       val curr_avg = brandSum / brandCount
       
-      val increaseRate = (curr_avg - prev_avg) * 100;
+      val increaseRate = if(prev_avg == 0.0) 0.0 else (curr_avg - prev_avg) * 100;
       
       val mongoBrandRuleConnection = MongoClient();
       var mongoBrandRuleColl = mongoBrandRuleConnection("Acme-Supermarket")("social_media_rules");
@@ -87,12 +78,14 @@ object TwitterScrapper {
       
         val brandNotification = MongoDBObject(
           "_type" -> "BrandNotification",
-          "percentageExceede" -> increaseRate,
+          "percentageExceeded" -> increaseRate,
           "brand_rule_id" -> rule.get("_id").toString(),
           "moment" -> new Date()
         )
         
         mongoBrandNotificationColl += brandNotification;
+        
+        log("Brand notification for rule" + rule.get("_id").toString(), "INFO");
         
         mongoBrandNotificationConnection.underlying.close();
         mongoBrandNotificationColl = null;
@@ -121,7 +114,7 @@ object TwitterScrapper {
       })
     });
     
-    var product_data = Map[Integer, Map[String, Any]]();
+    val product_data = Map[Integer, Map[String, Any]]();
     
     val mongoProductConnection = MongoClient();
     var mongoProductColl = mongoProductConnection("Acme-Supermarket")("products");
@@ -134,84 +127,107 @@ object TwitterScrapper {
     val stopWords = new StopWords();
     
     products.foreach { product => 
-        val p_id = product.get("_id");
-        var p_name = product.get("name").toString().toLowerCase().split(" ");
+      val p_id = Integer.parseInt(product.get("_id").toString());
+      var p_name = product.get("name").toString().toLowerCase().split(" ");
+      
+      p_name = stopWords.filter_stopwords(p_name, Languages.Any);
+      
+      val productStatuses = statuses.filter(status => p_name.exists(word => status._1.matches("\\b" + word.replaceAll("[~!@#$^%&*\\(\\)_+={}\\[\\]|;:\"'<,>.?`/\\\\-]", "") + "\\b")) )
+      
+      productStatuses.count().foreachRDD((rdd, time) => {
+        var prev_avg = 0.0;
+        var curr_avg = 0.0;
         
-        p_name = stopWords.filter_stopwords(p_name, Languages.Any);
-        
-        val productStatuses = statuses.filter(status => p_name.exists(word => status._1.matches("\\b" + word.replaceAll("[~!@#$^%&*\\(\\)_+={}\\[\\]|;:\"'<,>.?`/\\\\-]", "") + "\\b")) )
-        
-        productStatuses.count().foreachRDD((rdd, time) => {
+        if(product_data.contains(p_id)) {
+          val product_values = product_data.get(p_id).get;
+          var product_sum = product_values.get("sum").get.asInstanceOf[Long];
+          var product_count = product_values.get("count").get.asInstanceOf[Double];
           
-        });
-    }
-    
-    statuses.foreachRDD( statusRDD => {
-      statusRDD.foreach( status => {
-        var stopWords = new StopWords();
-        //println(status._1);
-        
-        val mongoProductConnection = MongoClient();
-        var mongoProductColl = mongoProductConnection("Acme-Supermarket")("products");
-        
-        val mongoBrandDataConnection = MongoClient();
-        var mongoBrandDataColl = mongoBrandDataConnection("Acme-Supermarket")("social_media_brand_data");
-        
-        val mongoProductDataConnection = MongoClient();
-        var mongoProductDataColl = mongoProductDataConnection("Acme-Supermarket")("social_media_product_data");
-        
-        val brandContained = status._1.toLowerCase().contains(config.getString("app.brand-name"));
-        val productContained = status._1.toLowerCase().contains(config.getString("app.product-name"));
-        
-        if(brandContained || productContained) {
-          //Introducir una entrada en la tabla de SocialMediaBrandData con el texto y la fecha
-          val brandOcurrence = MongoDBObject(
-              "date" -> (status._2.getTime / 1000L).toString(),
-              "description" -> status._1
-          );
+          prev_avg = product_sum / product_count;
           
-          mongoBrandDataColl += brandOcurrence;
+          product_sum += rdd.first();
+          product_count += 1.0;
           
-          log("Ocurrencia de marca", "INFO");
+          curr_avg = product_sum / product_count;
+          
+          product_values.put("sum", product_sum);
+          product_values.put("count", product_count);
+          
+          product_data.put(p_id, product_values);
+        } else {
+          val product_values = Map[String, Any]();
+          val product_sum = rdd.first();
+          val product_count = 1.0;
+          
+          curr_avg = product_sum / product_count;
+          
+          product_values.put("sum", product_sum);
+          product_values.put("count", product_count);
+          
+          product_data.put(p_id, product_values);
         }
         
-        //Por cada producto, mirar si el tweet contiene al nombre del mismo y, en caso afirmativo, 
-        //meter una entrada en la tabla SocialMediaProductData con el texto y la fecha del tweet y la ID del producto        
-        val query = MongoDBObject.empty;
-        val fields = MongoDBObject("_id" -> 1, "name" -> 1);
-        val products = mongoProductColl.find(query, fields);
-                
-        products.foreach { product => {
-          val p_id = product.get("_id");
-          var p_name = product.get("name").toString().toLowerCase().split(" ");
+        val increaseRate = if(prev_avg == 0.0) 0.0 else (curr_avg - prev_avg) * 100;
+        
+        val mongoProductRuleConnection = MongoClient();
+        var mongoProductRuleColl = mongoProductRuleConnection("Acme-Supermarket")("social_media_rules");
+        
+        val query = MongoDBObject("_type" -> "ProductRule", "increaseRate" -> MongoDBObject( "$le" -> increaseRate));
+        val fields = MongoDBObject("_id" -> 1);
+        val productRules = mongoProductRuleColl.find(query, fields);
+        
+        productRules.foreach { rule => 
+          val mongoProductNotificationConnection = MongoClient();
+          var mongoProductNotificationColl = mongoProductNotificationConnection("Acme-Supermarket")("social_media_notifications");
+        
+          val productNotification = MongoDBObject(
+            "_type" -> "ProductNotification",
+            "percentageExceeded" -> increaseRate,
+            "product_rule_id" -> rule.get("_id").toString(),
+            "moment" -> new Date()
+          )
           
-          p_name = stopWords.filter_stopwords(p_name, Languages.Any)
+          mongoProductNotificationColl += productNotification;
           
-          if(p_name.exists(status._1.contains)) {
-            val productOcurrence = MongoDBObject(
-              "date" -> (status._2.getTime / 1000L).toString(),
-              "description" -> status._1,
-              "product_id" -> p_id
-            );
-            
-            mongoProductDataColl += productOcurrence;
-            log("Ocurrencia de producto", "INFO");
-          }
-        }}
+          log("Product notification for rule" + rule.get("_id").toString(), "INFO");
+          
+          mongoProductNotificationConnection.underlying.close();
+          mongoProductNotificationColl = null;
+        }
         
-        //Clean-up
-        mongoProductConnection.underlying.close();
-        mongoProductColl = null;
-        
-        mongoBrandDataConnection.underlying.close();
-        mongoBrandDataColl = null;
-        
-        mongoProductDataConnection.underlying.close();
-        mongoProductDataColl = null;
-        
-        stopWords = null;
+        mongoProductRuleConnection.underlying.close();
+        mongoProductRuleColl = null;
       });
-    });
+      
+      productStatuses.foreachRDD( productRDD => {
+        productRDD.foreach(product => {
+          val mongoProductDataConnection = MongoClient();
+          var mongoProductDataColl = mongoProductDataConnection("Acme-Supermarket")("social_media_product_data");
+          
+          val productOcurrence = MongoDBObject(
+              "date" -> new Date(),
+              "description" -> product._1,
+              "product_id" -> p_id
+          );
+          
+          mongoProductDataColl += productOcurrence;
+          
+          mongoProductDataConnection.underlying.close();
+          mongoProductDataColl = null;
+          
+          log("Ocurrencia de producto", "INFO");
+        })
+      })
+    }
+    
+    sys.addShutdownHook({
+      println("ShutdownHook called")
+      ssc.stop(true, true)
+
+      while (!ssc.getState().equals(StreamingContextState.STOPPED)) {
+        println("Stopping... - " +  ssc.getState().toString())
+      }
+    })
     
     ssc.start()   
     ssc.awaitTermination()
